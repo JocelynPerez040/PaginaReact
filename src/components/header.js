20cbc3de063f87fd95ea8174f52088a5f1031a09
JocelynPerez040 <85253836+JocelynPerez040@users.noreply.github.com>
2022-10-26 10:59:45 -0500

    Se agrega la primer actualización de la página al repositorio con header y módulo de galaxia.

---
+++ src/components/header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../css/style.css';
+
+const PrimeraParte =()=>{
+    return(
+        <section class="begin">
+            <header class="header">
+                <img src="images/icono.png" alt=""/>
+                <nav>
+                    <ul>
+                        <a href="#inicio">INICIO</a>
+                        <a href="#galaxia">GALAXIA</a>
+                        <a href="#planetas">PLANETAS</a>
+                        <a href="#contelaciones">CONSTELACIONES</a>
+                        <a href="#curiosidades">CURIOSIDADES</a>
+                        <a href="#actividades">ACTIVIDADES</a>
+                    </ul>
+                </nav>
+            </header>        
+        </section>
+    )
+}
+
+export default PrimeraParte;
